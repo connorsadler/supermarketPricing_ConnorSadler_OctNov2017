7e866ea2ad44389ff5c414bd1d823fa2bc2fb402
Connor <Connor@DESKTOP-IF0PH2C>
2017-10-31 19:58:30 +0000

    first commit

---
+++ supermarketPricing_ConnorSadler_OctNov2017/test/connorsadler/supermarketpricing/Test1.java
@@ -0,0 +1,15 @@
+package connorsadler.supermarketpricing;
+
+import static org.junit.Assert.*;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+public class Test1 {
+
+	@Test
+	public void test1() {
+		assertThat("Should be 1.33", 1.33, CoreMatchers.is(1.33));
+	}
+
+}
