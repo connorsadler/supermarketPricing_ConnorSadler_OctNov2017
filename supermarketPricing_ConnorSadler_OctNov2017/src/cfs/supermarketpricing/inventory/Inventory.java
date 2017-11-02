e97da298bf08865d111c3bf8c055763abf9bdaa6
Connor <Connor@DESKTOP-IF0PH2C>
2017-11-02 20:29:48 +0000

    Inventory idea plus more SKU comments

---
+++ supermarketPricing_ConnorSadler_OctNov2017/src/cfs/supermarketpricing/inventory/Inventory.java
@@ -0,0 +1,16 @@
+package cfs.supermarketpricing.inventory;
+
+/**
+ * Inventory
+ * 
+ * TODO: This is for future enhancement
+ * 
+ * Inventory of items available to buy
+ * Each entry would have a number against it to tell us what the current stock level is
+ * This entry would link to a StockKeepingUnit
+ * 
+ * @author Connor
+ */
+public interface Inventory {
+
+}
--- supermarketPricing_ConnorSadler_OctNov2017/src/cfs/supermarketpricing/sku/StockKeepingUnit.java
+++ supermarketPricing_ConnorSadler_OctNov2017/src/cfs/supermarketpricing/sku/StockKeepingUnit.java
@@ -10,13 +10,15 @@ import cfs.supermarketpricing.money.MonetaryAmount;
  * Says nothing about how many of such an item we have in stock
  * An instance of this class will relate to many physical items in stock
  * 
- * Shamelessly taken from wikipedia:
+ * Shamelessly taken from Google:
  *   A stock keeping unit (SKU) is a product and service identification code for a store or product, often 
  *   portrayed as a machine-readable bar code that helps track the item for inventory. 
  *   A stock keeping unit (SKU) does not need to be assigned to physical products in inventory.
  */
 public interface StockKeepingUnit {
 	/**
+	 * getPrice
+	 * 
 	 * Price for a single item of this SKU
 	 */
 	public MonetaryAmount getPrice();
